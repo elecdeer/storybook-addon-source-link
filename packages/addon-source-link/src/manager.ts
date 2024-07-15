9b539e3285e03c311f433a9bb0b8e17b6b5dc02c
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-07-15 15:38:06 +0900

    fix: Fix addon's tool title

--- packages/addon-source-link/src/manager.ts
+++ packages/addon-source-link/src/manager.ts
@@ -7,7 +7,7 @@ addons.register(ADDON_ID, () => {
 	// Register the tool
 	addons.add(TOOL_ID, {
 		type: types.TOOL,
-		title: "My addon",
+		title: "Source Link",
 		match: ({ viewMode }) => !!viewMode?.match(/^(story|docs)$/),
 		render: Tool,
 	});
